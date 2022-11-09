7e3d0bf4619a8d38a5bb957cfaa68fc56e32fe59
mep3ab4ik <mep3ab4ik1@gmail.com>
2022-11-09 21:42:48 +0300

    Add models and  conf admin panel

---
+++ app/models.py
@@ -0,0 +1,92 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class TypeCompany(models.Model):
+    type = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name = "Type Company"
+        verbose_name_plural = "Type Company's"
+
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=128)
+    model = models.CharField(max_length=64)
+    release_data = models.DateField()
+
+    def __str__(self):
+        return f'{self.name} {self.model}'
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    type_company = models.ForeignKey(TypeCompany, on_delete=models.SET_NULL, null=True)
+    email = models.EmailField(max_length=64)
+    county = models.CharField(max_length=64)
+    city = models.CharField(max_length=32)
+    street = models.CharField(max_length=64)
+    house_number = models.CharField(max_length=16)
+    supplier = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True, blank=True)
+    debt = models.DecimalField(max_digits=11, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    level = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Company's"
+
+
+class CompanyStaff(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    staff = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.company.name
+
+    class Meta:
+        verbose_name_plural = "Company Staff"
+
+
+class CompanyProducts(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        verbose_name_plural = "Company Products"
+
+class Suppliers(models.Model):
+    class Meta:
+        unique_together = (('provider', 'seller'),)
+        verbose_name = "Supplier"
+        verbose_name_plural = "Suppliers"
+
+    provider = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, related_name='provider')
+    seller = models.ForeignKey(Company, on_delete=models.PROTECT, related_name='seller')
+    price = models.DecimalField(max_digits=11, decimal_places=2)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.price < 0:
+            self.price = self.price * -1
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.provider} - {self.seller}'
+
+
